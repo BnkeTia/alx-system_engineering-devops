fdedd6733fc809420c8dcbe6311d4956597847ec
BnKeTia <bernardnketia51@gmail.com>
2023-11-29 01:21:47 +0000

    call me maybe is the alt for phine number

--- 0x06-regular_expressions/3-pet_token.rb
+++ 0x06-regular_expressions/3-pet_token.rb
@@ -1,3 +1,3 @@
 #!/usr/bin/env ruby
 # A ruby script that accepts arguments nd matches the specified pattern.
-puts ARGV[0] =~ /^(hbt{2,5}n)$/ ? ARGV[0] : 'No match found.'
+puts ARGV[0] =~ /^(hbt{2,5}n)$/ ? ARGV[0] : ""
--- 0x06-regular_expressions/6-phon_num.rb
+++ 0x06-regular_expressions/6-phon_num.rb
@@ -1,4 +1,4 @@
 #!/usr/bin/env ruby
 # A regular expression must match a 10 digit phone number
 
-puts ARGV[0] =~ /^\d{10}$/ ? "#{ARGV[0]}" : ""
+puts ARGV[0].scan(/^[0-9]{10}$/).join
