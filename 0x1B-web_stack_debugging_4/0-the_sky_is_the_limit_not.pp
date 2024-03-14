c2744fa5633f2a34fc91a4ed46a0fc748927a09b
BnKeTia <bernardnketia51@gmail.com>
2024-03-14 11:25:01 +0000

    the sky is the limit

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,6 @@
+# A script that increases the number of requests Nginx web server can take
+exec { 'Increase Nginx ULIMIT':
+  command => 'sudo sed -i "s/15/4096/" /etc/default/nginx; sudo service nginx restart',
+  path    => ['/usr/bin', '/bin'],
+  returns => [0, 1]
+}
